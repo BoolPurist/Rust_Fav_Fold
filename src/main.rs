a1bcb6c38c8cc1343a6c86e7a94f5b2ccb787b7b
BoolPurist <fnaumann@posteo.de>
2023-01-02 09:24:07 +0100

    Refacture removed redundant error arm in handle_subcommand fn

--- src/main.rs
+++ src/main.rs
@@ -4,73 +4,68 @@ use clap::Parser;
 use colored::*;
 
 use folder_favorite::{cli_args::Commands, command_handling, linux_clipboard};
-
+type AppError = Result<(), Box<dyn Error>>;
 fn main() {
     if let Err(error) = linux_clipboard::execute_as_possible_daemon_clipboard() {
         exit_with_error(&*error);
     }
 
     let args = Commands::parse();
-    handle_subcommand(&args);
+    if let Err(error) = handle_subcommand(&args) {
+        exit_with_error(&*error);
+    }
 }
 
-fn handle_subcommand(sub_commands: &Commands) {
+fn handle_subcommand(sub_commands: &Commands) -> AppError {
     return match sub_commands {
         Commands::Set {
             name_favorite,
             new_path,
-        } => match command_handling::set_favorite_data(&name_favorite, &new_path) {
-            Ok(_) => (),
-            Err(error) => exit_with_error(&*error),
-        },
+        } => {
+            let _ = command_handling::set_favorite_data(&name_favorite, &new_path)?;
+
+            Ok(())
+        }
         Commands::Get { name, clipboard } => match name.as_ref() {
             Some(get_name) => {
-                let favorite = command_handling::get_fav(&get_name);
-
-                match favorite {
-                    Ok(read) => {
-                        put_into_clipboard_or_print(read.get_path(), *clipboard);
-                    }
-                    Err(error) => exit_with_error(&*error),
-                }
+                let favorite = command_handling::get_fav(&get_name)?;
+                put_into_clipboard_or_print(favorite.get_path(), *clipboard);
+                return Ok(());
             }
-            None => match command_handling::get_all_fav_table(*clipboard) {
-                Ok(table) => put_into_clipboard_or_print(&table, *clipboard),
-                Err(error) => exit_with_error(&*error),
-            },
+            None => {
+                let table = command_handling::get_all_fav_table(*clipboard)?;
+                put_into_clipboard_or_print(&table, *clipboard);
+                return Ok(());
+            }
         },
         Commands::Delete { name_favorite } => {
-            match command_handling::remove_from_fav(name_favorite) {
-                Ok(_) => (),
-                Err(error) => exit_with_error(&*error),
-            }
+            let _ = command_handling::remove_from_fav(&name_favorite)?;
+
+            Ok(())
         }
         Commands::Rename {
             old_name_favorite,
             new_name_favorite,
-        } => match command_handling::rename_fav(&old_name_favorite, &new_name_favorite) {
-            Ok(_) => (),
-            Err(error) => exit_with_error(&*error),
-        },
+        } => {
+            let _ = command_handling::rename_fav(&old_name_favorite, &new_name_favorite)?;
+            Ok(())
+        }
         Commands::PwdSet { name_favorite } => {
-            match command_handling::set_label_to_cwd(&name_favorite) {
-                Ok(_) => (),
-                Err(error) => exit_with_error(&*error),
-            }
+            let _ = command_handling::set_label_to_cwd(&name_favorite)?;
+            Ok(())
         }
     };
-
-    fn put_into_clipboard_or_print(content: &str, clipboard: bool) {
-        if clipboard {
-            if let Err(clip_error) = linux_clipboard::put_into_clipboard(content) {
-                exit_with_error(&*clip_error)
-            }
-        } else {
-            println!("{content}")
-        };
-    }
 }
 
+fn put_into_clipboard_or_print(content: &str, clipboard: bool) {
+    if clipboard {
+        if let Err(clip_error) = linux_clipboard::put_into_clipboard(content) {
+            exit_with_error(&*clip_error)
+        }
+    } else {
+        println!("{content}")
+    };
+}
 fn exit_with_error(message: &dyn Error) {
     let red_msg = format!("Error: {message}").red().to_string();
     eprint!("{red_msg}");
